6fd8b9c04d0af077bd6d24951ed0c715c5547bc8
bprlab <bprlab@10.201.36.209>
2017-11-11 13:56:12 +0300

    Hesaplamalar

---
+++ MavenProjeOrnekParent/Hafta4/src/main/java/Hesaplama.java
@@ -0,0 +1,12 @@
+
+public class Hesaplama {
+
+	public static void main(String[] args) {
+		int sonuc = topla(19, 25);
+		System.out.println(sonuc);
+	}
+	
+	public static int topla(int sayi1, int sayi2) {
+		return sayi1 + sayi2;
+	}
+}
