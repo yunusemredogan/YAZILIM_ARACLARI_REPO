325a95903d72299f98dcc016ddc096d7cb890141
bprlab <bprlab5934>
2017-11-18 13:09:46 +0300

    String Length Methodu Kullanıldı...

---
+++ MavenProjeOrnekParent/Hafta4/src/main/java/StringLength.java
@@ -0,0 +1,16 @@
+/**
+ * Created by iskenderuzuner on 18.11.2017.
+ */
+import java.io.*;
+
+public class StringLength {
+
+    public static void main(String[] args) {
+        System.out.print("String Length :" );
+        System.out.println(Str1.length());
+
+        System.out.print("String Length :" );
+        System.out.println(Str2.length());
+    }
+
+}
